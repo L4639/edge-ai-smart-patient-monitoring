f07a0f62a414074a962a9b62c828921086d49da2
Lahari B A <balahari582@gmail.com>
2026-07-06 13:02:10 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,15 +1,11 @@
-# =============================
-# Fix import path (VERY IMPORTANT)
-# =============================
+
 import os
 import sys
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(BASE_DIR)
 
-# =============================
 # Imports
-# =============================
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -17,35 +13,26 @@ import time
 
 from backend.inference.predictor import RLAlertPredictor
 
-
-# =============================
 # Page Setup
-# =============================
 st.set_page_config(page_title="Edge AI Patient Monitor", layout="wide")
 
-st.title("🧠 Edge AI Smart Patient Monitoring System")
+st.title("Edge AI Smart Patient Monitoring System")
 st.markdown("### Reinforcement Learning Based Clinical Alert Optimization")
 
 model = RLAlertPredictor()
 
-# =============================
-# Sidebar Controls
-# =============================
+# Sidebar Control
 st.sidebar.header("Mode Selection")
 
 mode = st.sidebar.radio("Choose Mode:", ["Simulation", "Manual Entry"])
 
-# =============================
 # Layout
-# =============================
 col1, col2 = st.columns(2)
 
 chart_placeholder = col1.empty()
 alert_placeholder = col2.empty()
 
-# ======================================================
-# 🔁 SIMULATION MODE
-# ======================================================
+#  SIMULATION MODE
 if mode == "Simulation":
 
     if st.sidebar.button("Start Simulation"):
@@ -67,17 +54,15 @@ if mode == "Simulation":
             action = model.predict(data)
 
             if action == 0:
-                alert_placeholder.success("✅ Normal")
+                alert_placeholder.success(" Normal")
             elif action == 1:
-                alert_placeholder.warning("⚠ Alert Nurse")
+                alert_placeholder.warning(" Alert Nurse")
             else:
-                alert_placeholder.error("🚨 Escalate Immediately")
+                alert_placeholder.error(" Escalate Immediately")
 
             time.sleep(0.5)
 
-# ======================================================
-# ✍ MANUAL ENTRY MODE
-# ======================================================
+# MANUAL ENTRY MODE
 else:
 
     st.sidebar.subheader("Enter Latest Vitals")
@@ -117,11 +102,11 @@ else:
     action = model.predict(st.session_state.history)
 
     if action == 0:
-        alert_placeholder.success("✅ Normal")
+        alert_placeholder.success(" Normal")
     elif action == 1:
-        alert_placeholder.warning("⚠ Alert Nurse")
+        alert_placeholder.warning(" Alert Nurse")
     else:
-        alert_placeholder.error("🚨 Escalate Immediately")
+        alert_placeholder.error(" Escalate Immediately")
         
     import pandas as pd
 
